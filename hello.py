14a97c1b6b9d4c0757ee6f66d63189e764ad408a
Daniel Pasacrita <dpasacrita@crownawards.com>
2018-05-07 16:26:43 -0400

    Testing running different things with different buttons.

--- hello.py
+++ hello.py
@@ -13,19 +13,11 @@ def hello_world():
 def hello(name=None):
     return render_template('hello.html', name=name)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
-
 @app.route('/button_test')
 def button_test():
     return render_template('button_test.html', url=SERVER_NAME, server_time=time.time())
+
+@app.route('/start/')
+@app.route('/start/<server>')
+def start(server=None):
+    return 'We\'re gonna restart server %s' % server
