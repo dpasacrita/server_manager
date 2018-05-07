92fd5fcd7b1d5312ba1a36b7470a182b9cb3568a
Daniel Pasacrita <dpasacrita@crownawards.com>
2018-05-07 15:01:21 -0400

    Testing login

--- hello.py
+++ hello.py
@@ -10,3 +10,16 @@ def hello_world():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
