3e8ac5fa55cc73858cc79b582c6612c2a33073e8
Daniel Pasacrita <dpasacrita@crownawards.com>
2018-05-07 13:03:47 -0400

    Fixing import statement again.

--- hello.py
+++ hello.py
@@ -9,4 +9,4 @@ def hello_world():
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return flask.render_template('hello.html', name=name)
+    return render_template('hello.html', name=name)
