431b2bf3d40474c5ab273c98a77eeddb345bf3d7
Daniel Pasacrita <dpasacrita@crownawards.com>
2018-06-08 14:09:21 -0400

    Beta4. Server-Status reader/bandwith measure.

--- server_manager.py
+++ server_manager.py
@@ -18,7 +18,7 @@ def prodbuilders():
 @app.route('/prodbuilders/serverstatus')
 def pbserverstatus(host='prodbuilder1'):
     stats = measure_bandwith.read_server_status(host)
-    return render_template('pbserverstatus.html', server_status=stats)
+    return render_template('prodbuilders/serverstatus.html', server_status=stats)
 
 
 @app.route('/testapis')
